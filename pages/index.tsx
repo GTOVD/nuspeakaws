7ce783eaa833c060c845ee693563275e8445dfd2
GTOVD <vickerst10@gmail.com>
2022-07-29 18:08:35 -0600

    logic to add items field to query

--- pages/index.tsx
+++ pages/index.tsx
@@ -7,8 +7,11 @@ import {
     ListCommunitiesQuery,
     Post,
     ListPostsQuery,
+    GetPostQuery,
+    GetVoteQuery,
+    Vote,
 } from "../src/API";
-import { listCommunities } from "../src/graphql/queries";
+import { getPost, getVote, listCommunities } from "../src/graphql/queries";
 import { listPosts } from "../src/graphql/queries";
 import Community from "../components/community";
 import { ThemeProvider } from "@emotion/react";
@@ -19,38 +22,58 @@ export default function Home() {
     const { user } = useUser();
     const [allCommunities, setCommunities] = useState<Communities[]>([]);
     const [allPosts, setPosts] = useState<Post[]>([]);
+    const [allPostsData, setPostsData] = useState<Post>();
 
-    useEffect(() => {
-        const fetchCommunitiesFromAPI = async (): Promise<Communities[]> => {
-            const allCommunities = (await API.graphql({
-                query: listCommunities,
-            })) as { data: ListCommunitiesQuery; errors: any[] };
-            if (allCommunities.data) {
-                setCommunities(
-                    allCommunities.data.listCommunities.items as Communities[]
-                );
-                return allCommunities.data.listCommunities
-                    .items as Communities[];
-            } else {
-                throw new Error("Could not get communities from API.");
-            }
-        };
-        fetchCommunitiesFromAPI();
-        const fetchPostsFromAPI = async (): Promise<Post[]> => {
-            const allPosts = (await API.graphql({
-                query: listPosts,
-            })) as { data: ListPostsQuery; errors: any[] };
-            if (allPosts.data) {
-                setPosts(allPosts.data.listPosts.items as Post[]);
-                return allPosts.data.listPosts.items as Post[];
+    const fetchPostsFromAPI = async (): Promise<Post[]> => {
+        const allPosts = (await API.graphql({
+            query: listPosts,
+        })) as { data: ListPostsQuery; errors: any[] };
+        console.log("all data", allPosts.data);
+        if (allPosts.data) {
+            setPosts(allPosts.data.listPosts.items as Post[]);
+            return allPosts.data.listPosts.items as Post[];
+        } else {
+            throw new Error("Could not get posts from API.");
+        }
+    };
+
+    const fetchCommunitiesFromAPI = async (): Promise<Communities[]> => {
+        const allCommunities = (await API.graphql({
+            query: listCommunities,
+        })) as { data: ListCommunitiesQuery; errors: any[] };
+        if (allCommunities.data) {
+            setCommunities(
+                allCommunities.data.listCommunities.items as Communities[]
+            );
+            return allCommunities.data.listCommunities.items as Communities[];
+        } else {
+            throw new Error("Could not get communities from API.");
+        }
+    };
+
+    const fetchIndividualPostsData = async (): Promise<void> => {
+        allPosts.forEach(async (post): Promise<void> => {
+            const postsData = (await API.graphql({
+                query: getPost,
+                variables: { id: post.id },
+            })) as {
+                data: GetPostQuery;
+            };
+            if (postsData.data) {
+                setPostsData(postsData.data.getPost as Post);
             } else {
                 throw new Error("Could not get posts from API.");
             }
-        };
+        });
+    };
+
+    useEffect(() => {
+        fetchCommunitiesFromAPI();
         fetchPostsFromAPI();
+        fetchIndividualPostsData();
     }, []);
 
-    console.log({ allCommunities, allPosts });
+    console.log({ allPosts, allPostsData });
 
     return (
         <ThemeProvider theme={theme}>
--- src/graphql/queries.ts
+++ src/graphql/queries.ts
@@ -3,258 +3,276 @@
 // this is an auto generated file. This will be overwritten
 
 export const getCommunities = /* GraphQL */ `
-  query GetCommunities($id: ID!) {
-    getCommunities(id: $id) {
-      id
-      name
-      posts {
-        items {
-          id
-          title
-          contents
-          link
-          createdAt
-          updatedAt
-          communitiesPostsId
-          owner
+    query GetCommunities($id: ID!) {
+        getCommunities(id: $id) {
+            id
+            name
+            posts {
+                items {
+                    id
+                    title
+                    contents
+                    link
+                    createdAt
+                    updatedAt
+                    communitiesPostsId
+                    owner
+                }
+                nextToken
+            }
+            createdAt
+            updatedAt
+            owner
         }
-        nextToken
-      }
-      createdAt
-      updatedAt
-      owner
     }
-  }
 `;
 export const listCommunities = /* GraphQL */ `
-  query ListCommunities(
-    $filter: ModelCommunitiesFilterInput
-    $limit: Int
-    $nextToken: String
-  ) {
-    listCommunities(filter: $filter, limit: $limit, nextToken: $nextToken) {
-      items {
-        id
-        name
-        posts {
-          nextToken
+    query ListCommunities(
+        $filter: ModelCommunitiesFilterInput
+        $limit: Int
+        $nextToken: String
+    ) {
+        listCommunities(filter: $filter, limit: $limit, nextToken: $nextToken) {
+            items {
+                id
+                name
+                posts {
+                    nextToken
+                }
+                createdAt
+                updatedAt
+                owner
+            }
+            nextToken
         }
-        createdAt
-        updatedAt
-        owner
-      }
-      nextToken
     }
-  }
 `;
 export const getPost = /* GraphQL */ `
-  query GetPost($id: ID!) {
-    getPost(id: $id) {
-      id
-      title
-      contents
-      link
-      votes {
-        items {
-          id
-          vote
-          createdAt
-          updatedAt
-          postVotesId
-          commentVotesId
-          owner
+    query GetPost($id: ID!) {
+        getPost(id: $id) {
+            id
+            title
+            contents
+            link
+            votes {
+                items {
+                    id
+                    vote
+                    createdAt
+                    updatedAt
+                    postVotesId
+                    commentVotesId
+                    owner
+                }
+                nextToken
+            }
+            communities {
+                id
+                name
+                posts {
+                    nextToken
+                }
+                createdAt
+                updatedAt
+                owner
+            }
+            comments {
+                items {
+                    id
+                    content
+                    createdAt
+                    updatedAt
+                    postCommentsId
+                    commentReplyId
+                    owner
+                }
+                nextToken
+            }
+            createdAt
+            updatedAt
+            communitiesPostsId
+            owner
         }
-        nextToken
-      }
-      communities {
-        id
-        name
-        posts {
-          nextToken
-        }
-        createdAt
-        updatedAt
-        owner
-      }
-      comments {
-        items {
-          id
-          content
-          createdAt
-          updatedAt
-          postCommentsId
-          commentReplyId
-          owner
-        }
-        nextToken
-      }
-      createdAt
-      updatedAt
-      communitiesPostsId
-      owner
     }
-  }
 `;
 export const listPosts = /* GraphQL */ `
-  query ListPosts(
-    $filter: ModelPostFilterInput
-    $limit: Int
-    $nextToken: String
-  ) {
-    listPosts(filter: $filter, limit: $limit, nextToken: $nextToken) {
-      items {
-        id
-        title
-        contents
-        link
-        votes {
-          nextToken
+    query ListPosts(
+        $filter: ModelPostFilterInput
+        $limit: Int
+        $nextToken: String
+    ) {
+        listPosts(filter: $filter, limit: $limit, nextToken: $nextToken) {
+            items {
+                id
+                title
+                contents
+                link
+                votes {
+                    items {
+                        id
+                        vote
+                        createdAt
+                        updatedAt
+                        postVotesId
+                        commentVotesId
+                        owner
+                    }
+                    nextToken
+                }
+                communities {
+                    id
+                    name
+                    createdAt
+                    updatedAt
+                    owner
+                }
+                comments {
+                    items {
+                        id
+                        content
+                        createdAt
+                        updatedAt
+                        postCommentsId
+                        commentReplyId
+                        owner
+                    }
+                    nextToken
+                }
+                createdAt
+                updatedAt
+                communitiesPostsId
+                owner
+            }
+            nextToken
         }
-        communities {
-          id
-          name
-          createdAt
-          updatedAt
-          owner
-        }
-        comments {
-          nextToken
-        }
-        createdAt
-        updatedAt
-        communitiesPostsId
-        owner
-      }
-      nextToken
     }
-  }
 `;
 export const getComment = /* GraphQL */ `
-  query GetComment($id: ID!) {
-    getComment(id: $id) {
-      id
-      post {
-        id
-        title
-        contents
-        link
-        votes {
-          nextToken
+    query GetComment($id: ID!) {
+        getComment(id: $id) {
+            id
+            post {
+                id
+                title
+                contents
+                link
+                votes {
+                    nextToken
+                }
+                communities {
+                    id
+                    name
+                    createdAt
+                    updatedAt
+                    owner
+                }
+                comments {
+                    nextToken
+                }
+                createdAt
+                updatedAt
+                communitiesPostsId
+                owner
+            }
+            reply {
+                items {
+                    id
+                    content
+                    createdAt
+                    updatedAt
+                    postCommentsId
+                    commentReplyId
+                    owner
+                }
+                nextToken
+            }
+            content
+            votes {
+                items {
+                    id
+                    vote
+                    createdAt
+                    updatedAt
+                    postVotesId
+                    commentVotesId
+                    owner
+                }
+                nextToken
+            }
+            createdAt
+            updatedAt
+            postCommentsId
+            commentReplyId
+            owner
         }
-        communities {
-          id
-          name
-          createdAt
-          updatedAt
-          owner
-        }
-        comments {
-          nextToken
-        }
-        createdAt
-        updatedAt
-        communitiesPostsId
-        owner
-      }
-      reply {
-        items {
-          id
-          content
-          createdAt
-          updatedAt
-          postCommentsId
-          commentReplyId
-          owner
-        }
-        nextToken
-      }
-      content
-      votes {
-        items {
-          id
-          vote
-          createdAt
-          updatedAt
-          postVotesId
-          commentVotesId
-          owner
-        }
-        nextToken
-      }
-      createdAt
-      updatedAt
-      postCommentsId
-      commentReplyId
-      owner
     }
-  }
 `;
 export const listComments = /* GraphQL */ `
-  query ListComments(
-    $filter: ModelCommentFilterInput
-    $limit: Int
-    $nextToken: String
-  ) {
-    listComments(filter: $filter, limit: $limit, nextToken: $nextToken) {
-      items {
-        id
-        post {
-          id
-          title
-          contents
-          link
-          createdAt
-          updatedAt
-          communitiesPostsId
-          owner
+    query ListComments(
+        $filter: ModelCommentFilterInput
+        $limit: Int
+        $nextToken: String
+    ) {
+        listComments(filter: $filter, limit: $limit, nextToken: $nextToken) {
+            items {
+                id
+                post {
+                    id
+                    title
+                    contents
+                    link
+                    createdAt
+                    updatedAt
+                    communitiesPostsId
+                    owner
+                }
+                reply {
+                    nextToken
+                }
+                content
+                votes {
+                    nextToken
+                }
+                createdAt
+                updatedAt
+                postCommentsId
+                commentReplyId
+                owner
+            }
+            nextToken
         }
-        reply {
-          nextToken
-        }
-        content
-        votes {
-          nextToken
-        }
-        createdAt
-        updatedAt
-        postCommentsId
-        commentReplyId
-        owner
-      }
-      nextToken
     }
-  }
 `;
 export const getVote = /* GraphQL */ `
-  query GetVote($id: ID!) {
-    getVote(id: $id) {
-      id
-      vote
-      createdAt
-      updatedAt
-      postVotesId
-      commentVotesId
-      owner
+    query GetVote($id: ID!) {
+        getVote(id: $id) {
+            id
+            vote
+            createdAt
+            updatedAt
+            postVotesId
+            commentVotesId
+            owner
+        }
     }
-  }
 `;
 export const listVotes = /* GraphQL */ `
-  query ListVotes(
-    $filter: ModelVoteFilterInput
-    $limit: Int
-    $nextToken: String
-  ) {
-    listVotes(filter: $filter, limit: $limit, nextToken: $nextToken) {
-      items {
-        id
-        vote
-        createdAt
-        updatedAt
-        postVotesId
-        commentVotesId
-        owner
-      }
-      nextToken
+    query ListVotes(
+        $filter: ModelVoteFilterInput
+        $limit: Int
+        $nextToken: String
+    ) {
+        listVotes(filter: $filter, limit: $limit, nextToken: $nextToken) {
+            items {
+                id
+                vote
+                createdAt
+                updatedAt
+                postVotesId
+                commentVotesId
+                owner
+            }
+            nextToken
+        }
     }
-  }
 `;
